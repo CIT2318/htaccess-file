1f45cf11962e8840de1cfd47176e234c8eef9e21
mmantle-hud <m.e.mantle@hud.ac.uk>
2017-01-30 08:33:39 +0000

    tidied up master

--- index.php
+++ index.php
@@ -1,37 +1,19 @@
-
 <?php
-
 require_once("models/film-model.php");
-$baseURL="http://localhost/CIT2318/htaccess/";
-$basePath="/CIT2318/htaccess/";
-
-//get full path of current URL
-$url=$_SERVER["REQUEST_URI"]; 
-echo "<p>URL: $url</p>";
-
-//get the end components of the path
-$shortPath = substr($url, strlen($basePath)); 
-echo "<p>Short path: $shortPath</p>";
-
-//split this string using '/' as a delimiter
-$urlArray=explode("/", $shortPath); 
-print_r($urlArray);
-
-//the first element in the array is the action to use
-$action=$urlArray[0];
-echo "<p>$action</p>";
-
+if(isset($_GET["action"])){
+    $action=$_GET['action'];
+}else{
+    $action="list";
+}
 if ($action==="list") {
 	$films=getAllFilms();
 	$pageTitle="List all films";
 	include("views/list-view.php");
-} else if ($action==="details" && isset($urlArray[1])) {
-	$film=getFilmById($urlArray[1]);
+} else if ($action==="details" && isset($_GET['id'])) {
+	$film=getFilmById($_GET['id']);
 	$pageTitle="Film details";
 	include("views/details-view.php");
 } else {
     include("views/404-view.php");
 }
-
-
 ?>
